19727bc2680746339117ab86a9c286743880d5f9
HRANT-77 <hloryan@mail.ru>
2020-11-06 22:07:33 -0800

    Message

---
+++ src/com/company/Main4.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main4 {
+    public static void main(String[] args) {
+        for (int i = -20; i < 61; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
